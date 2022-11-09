d2d44576129f8bab8bf2ad2b92a648c7d7f1035e
Trong Hieu Vuong <hieuvg02@gmail.com>
2022-11-08 22:35:55 -0800

    fix.

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -12,7 +12,7 @@ const firebase = require("firebase/app");
 require("firebase/auth");
 require("firebase/firestore");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("../../../serviceAccountKey.json");
 
 const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
